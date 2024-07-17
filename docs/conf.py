e1122676f9e4c2011d87ed761934e671f2feda62
Nina Brown <103523018+brownn11@users.noreply.github.com>
2024-07-17 12:21:34 -0500

    Update conf.py
    changed path

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../spectra_ez_download'))
 
 project = 'spectra_ez_download'
 copyright = '2024, Kate Hobbs, Zoë de Beurs, Nina Brown'
