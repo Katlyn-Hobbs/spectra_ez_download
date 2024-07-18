75726475674865f65183a15d3a4a054628f8efb1
Nina Brown <103523018+brownn11@users.noreply.github.com>
2024-07-18 15:48:57 -0500

    Update ez_spectral_analysis.py
    rearranged into class format
    
    added interpolation functionality
    
    added more dependencies

--- spectra_ez_download/ez_spectral_analysis.py
+++ spectra_ez_download/ez_spectral_analysis.py
@@ -9,154 +9,182 @@ from specutils.fitting import fit_generic_continuum
 from astropy import units as u
 import astropy.constants as ac
 from astropy.modeling import models
+from scipy.interpolate import interp1d
+from scipy import interpolate
 
-def spectra_read(path, instrument):
+class ez_spectra_analysis(object):
     """
-    Read spectra
-
-    Open fits files and return the wavelength and spectra for each wavelength order.
+    Completes simple spectral analysis for user-uploaded solar spectra of HARPS-N.
 
     Args:
-        path (str): string. Specified path of where a spectrum is located
-        instrument (str): String that specifies the instrument. Currently, the only supported instrumented is 'HARPS-N'.
-
-    Returns:
-        arrays: wavelength array, flux array
-
+        path: path to local data
+        orders (list, optional): list of orders to be inspected. Default is [16, 17, 18]. Beware of noisy orders and tellurics.
+        plot (bool, optional): produce plots from analysis. 
     """
 
-    if instrument =='HARPS-N':
-      flux_col = 1, wl_col = 4
-      hdul = fits.open(path)
-      flux_data = hdul[flux_col].data
-      wavelength_data = hdul[wl_col].data
-      return wavelength_data, flux_data
-    else:
-       print("Error: "+instrument+" is currently not supported by spectra_ez_download. Please submit an issue on github.")
-
-
-
-
-def plot_raw_data(path, orders=[16, 17, 18]):
-    wavelengths, fluxes =spectra_read(path=path)
-
-    fig, axes = plt.subplots(len(orders), 1,figsize=(9, len(orders)*2))
-    for index in range(0, len(orders)):
-
-        ax = axes[index]
-        ax.plot(wavelengths[orders[index]], fluxes[orders[index]], label='order = '+str(orders[index]),
-                color='purple')
-        ax.set_ylabel("Flux")
-        ax.set_title("Order = "+str(orders[index]))
+    def __init__(self, path, instrument, orders = [16, 17, 18], plot = False, median_window = 3):
+        self.path = path
+        self.orders = orders
+        self.plot = plot
+        self.instrument = instrument
+        self.median_window = median_window
+        
+        
+    def spectra_read(self):
+        """
+        Read spectra
     
-    ax.set_xlabel(r"Wavelength ($\AA$)")
-    plt.tight_layout()
-    plt.show()
-
-
-def continuum_norm(path, plot=False, median_window=3, orders=None):
-  """
-  normalize the continuum of spectra
-
-    Opens fits files, cycles through each wavelength order (or the user-specified orders) and normalizes the continuum using specutils
-
-    Args:
-        path (str): string. Specified path of where a spectrum is located
-        plot (bool): boolean. If True, plots will be generated. Default is False
-        orders (array): array. array of wavelength orders to be normalized. If not specified, all wavelength orders will be used.
-        median_window (int): integer. Needs to be an odd integer. The width of the median smoothing kernel used to filter the data before fitting the continuum.
-
-    Returns:
-        arrays: wavelength array, normalized flux array
-  """
-
-  wavelengths, fluxes =spectra_read(path=path)
-
-  norm_fluxes = []
-  
-  if orders == None:
-    for order in range(0, len(fluxes)):
-      spectrum = Spectrum1D(flux=fluxes[order].astype(np.float64)*u.Jy, 
-                            spectral_axis=wavelengths[order].astype(np.float64)/10*u.nm)
-      continuum_g_x = fit_generic_continuum(spectrum, median_window=median_window, 
-                                            model=models.Chebyshev1D(4, c0=0., c1=0., c2=0., c3=0.))
-      y_continuum_fitted = continuum_g_x(wavelengths[order]/10*u.nm)
-
-      spec_normalized = spectrum / y_continuum_fitted
-      norm_fluxes.append(spec_normalized.flux)
-
-      if plot:
-        f, axes = plt.subplots(2, 1, figsize=(12, 4))
-        ax = axes[0]  
-        ax.plot(wavelengths[order]/10*u.nm, fluxes[order]*u.Jy, color='purple')  
-        ax.plot(wavelengths[order]/10*u.nm, y_continuum_fitted)  
-        ax.set_ylabel('Flux')  
-        ax.set_title("Continuum Fitting")
-
-        ax = axes[1]
-        ax.plot(spec_normalized.spectral_axis, spec_normalized.flux, color='purple')    
-        ax.set_title("Continuum normalized spectrum")
-        ax.set_ylabel('Normalized flux')  
-        ax.set_xlabel('Wavelengths (nm)')  
-        f.tight_layout 
-  else:
-    for order in orders:
-      spectrum = Spectrum1D(flux=fluxes[order].astype(np.float64)*u.Jy, 
-                            spectral_axis=wavelengths[order].astype(np.float64)/10*u.nm)
-      continuum_g_x = fit_generic_continuum(spectrum, median_window=median_window, 
-                                            model=models.Chebyshev1D(4, c0=0., c1=0., c2=0., c3=0.))
-      y_continuum_fitted = continuum_g_x(wavelengths[order]/10*u.nm)
-
-      spec_normalized = spectrum / y_continuum_fitted
-      norm_fluxes.append(spec_normalized.flux)
-
-      if plot:
-        f, axes = plt.subplots(2, 1, figsize=(12, 4))
-        ax = axes[0]  
-        ax.plot(wavelengths[order]/10*u.nm, fluxes[order]*u.Jy, color='purple')  
-        ax.plot(wavelengths[order]/10*u.nm, y_continuum_fitted)
-        ax.set_ylabel('Flux')   
-        ax.set_title("Continuum Fitting")
-
-        ax = axes[1]
-        ax.plot(spec_normalized.spectral_axis, spec_normalized.flux, color='purple')
-        ax.set_xlabel('Wavelengths (nm)')
-        ax.set_ylabel('Normalized flux')        
-        ax.set_title("Continuum normalized spectrum")
-        f.tight_layout    
-
-  return wavelengths.astype(np.float64), norm_fluxes        
-
-def BC_correction():
-    """
-    Apply a barycentric correction
+        Open fits files and return the wavelength and spectra for each wavelength order.
     
-      Apply a barycentric correction to wavlelength values based on Barycentric velocities read from .fits header.
+        Args:
+            path (str): string. Specified path of where a spectrum is located
+            instrument (str): String that specifies the instrument. Currently, the only supported instrumented is 'HARPS-N'.
+    
+        Returns:
+            arrays: wavelength array, flux array
+        """
+    
+        if self.instrument =='HARPS-N':
+          self.flux_col = 1, self.wl_col = 4
+          hdul = fits.open(self.path)
+          flux_data = hdul[flux_col].data
+          wavelength_data = hdul[wl_col].data
+          return wavelength_data, flux_data
+        else:
+           print("Error: "+self.instrument+" is currently not supported by spectra_ez_download. Please submit an issue on github.")
 
-      Args:
-          none
-
-      Returns:
-          arrays: corrected wavelengths, flux
-    """
-    # load in wavelength and flux:
-    wavelength, flux = continuum_norm(path)
-    # load in BERV and BJD from header:
-    hdul = fits.open(path)
-    hdr = hdul[0].header
-    berv = hdr['*BERV'][0] # km/s
-    bjd = hdr['*BJD'][0] # Barycentric Julian date (TDB)
-
-    rv_corr = 1 + berv/ac.c # corrective value
-    wavelength_corrected = wavelength*rv_corr # angstroms
-
-    return wavelength_corrected, flux
-         
-
+    def plot_raw_data(self):
+        wavelengths, fluxes = ez_spectral_analysis.spectra_read()
+    
+        fig, axes = plt.subplots(len(self.orders), 1,figsize=(9, len(self.orders)*2))
+        for index in range(0, len(self.orders)):
+    
+            ax = axes[index]
+            ax.plot(wavelengths[self.orders[index]], fluxes[self.orders[index]], label='order = '+str(self.orders[index]),
+                    color='purple')
+            ax.set_ylabel("Flux")
+            ax.set_title("Order = "+str(self.orders[index]))
+        
+        ax.set_xlabel(r"Wavelength ($\AA$)")
+        plt.tight_layout()
+        plt.show()
+    
+    
+    def continuum_norm(self):
+      """
+      normalize the continuum of spectra
+    
+        Opens fits files, cycles through each wavelength order (or the user-specified orders) and normalizes the continuum using specutils
+    
+        Args:
+            path (str): string. Specified path of where a spectrum is located
+            plot (bool): boolean. If True, plots will be generated. Default is False
+            orders (array): array. array of wavelength orders to be normalized. If not specified, all wavelength orders will be used.
+            median_window (int): integer. Needs to be an odd integer. The width of the median smoothing kernel used to filter the data before fitting the continuum.
+    
+        Returns:
+            arrays: wavelength array, normalized flux array
+      """
+    
+      wavelengths, fluxes = ez_spectral_analysis.spectra_read()
+    
+      norm_fluxes = []
       
+      if self.orders == None:
+        for order in range(0, len(fluxes)):
+          spectrum = Spectrum1D(flux=fluxes[order].astype(np.float64)*u.Jy, 
+                                spectral_axis=wavelengths[order].astype(np.float64)/10*u.nm)
+          continuum_g_x = fit_generic_continuum(spectrum, median_window=self.median_window, 
+                                                model=models.Chebyshev1D(4, c0=0., c1=0., c2=0., c3=0.))
+          y_continuum_fitted = continuum_g_x(wavelengths[order]/10*u.nm)
+    
+          spec_normalized = spectrum / y_continuum_fitted
+          norm_fluxes.append(spec_normalized.flux)
+    
+          if self.plot:
+            f, axes = plt.subplots(2, 1, figsize=(12, 4))
+            ax = axes[0]  
+            ax.plot(wavelengths[order]/10*u.nm, fluxes[order]*u.Jy, color='purple')  
+            ax.plot(wavelengths[order]/10*u.nm, y_continuum_fitted)  
+            ax.set_ylabel('Flux')  
+            ax.set_title("Continuum Fitting")
+    
+            ax = axes[1]
+            ax.plot(spec_normalized.spectral_axis, spec_normalized.flux, color='purple')    
+            ax.set_title("Continuum normalized spectrum")
+            ax.set_ylabel('Normalized flux')  
+            ax.set_xlabel('Wavelengths (nm)')  
+            f.tight_layout() 
+      else:
+        for order in self.orders:
+          spectrum = Spectrum1D(flux=fluxes[order].astype(np.float64)*u.Jy, 
+                                spectral_axis=wavelengths[order].astype(np.float64)/10*u.nm)
+          continuum_g_x = fit_generic_continuum(spectrum, median_window=self.median_window, 
+                                                model=models.Chebyshev1D(4, c0=0., c1=0., c2=0., c3=0.))
+          y_continuum_fitted = continuum_g_x(wavelengths[order]/10*u.nm)
+    
+          spec_normalized = spectrum / y_continuum_fitted
+          norm_fluxes.append(spec_normalized.flux)
+    
+          if self.plot:
+            f, axes = plt.subplots(2, 1, figsize=(12, 4))
+            ax = axes[0]  
+            ax.plot(wavelengths[order]/10*u.nm, fluxes[order]*u.Jy, color='purple')  
+            ax.plot(wavelengths[order]/10*u.nm, y_continuum_fitted)
+            ax.set_ylabel('Flux')   
+            ax.set_title("Continuum Fitting")
+    
+            ax = axes[1]
+            ax.plot(spec_normalized.spectral_axis, spec_normalized.flux, color='purple')
+            ax.set_xlabel('Wavelengths (nm)')
+            ax.set_ylabel('Normalized flux')        
+            ax.set_title("Continuum normalized spectrum")
+            f.tight_layout()    
+    
+      return wavelengths.astype(np.float64), norm_fluxes        
+    
+    def BC_correction(self):
+        """
+        Apply a barycentric correction
+        
+          Apply a barycentric correction to wavlelength values based on Barycentric velocities read from .fits header.
+    
+          Args:
+              none
+    
+          Returns:
+              arrays: corrected wavelengths, flux
+        """
+        # load in wavelength and flux:
+        wavelength, flux = ez_spectral_analysis.continuum_norm()
+        # load in BERV and BJD from header:
+        hdul = fits.open(self.path)
+        hdr = hdul[0].header
+        berv = hdr['*BERV'][0] # km/s
+        bjd = hdr['*BJD'][0] # Barycentric Julian date (TDB)
+    
+        rv_corr = 1 + berv/ac.c # corrective value
+        wavelength_corrected = wavelength*rv_corr # angstroms
+    
+        return wavelength_corrected, flux
 
+    def interpolation(self, template):   
+        berv_wl, berv_flux = ez_spectra_analysis.BC_correction()
+        hdul = fits.open(template)
 
-
+        wavelength_template = hdul[self.wl_col].data
+        flux_interp_segment = []
+        for order in range(len(berv_wl)):
+            spline_coeff = interpolate.splrep(berv_wl[order], berv_flux[order])
+            flux_interp_segment.append(interpolate.splev(wavelength_template[order], spline_coeff))
+    
+        return berv_wl, flux_interp_segment
+             
+    
+          
+    
+    
+    
 
 
 
