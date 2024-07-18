33520e8571a4b1e1cd2ccee1c1e4099a2470acc6
Zoe dB <41705417+zdebeurs@users.noreply.github.com>
2024-07-18 13:50:17 -0500

    add angstrom test

--- spectra_ez_download/tests/test_unit_angstrom.py
+++ spectra_ez_download/tests/test_unit_angstrom.py
@@ -1,10 +1,19 @@
+import os
+import sys
+import inspect
+import numpy as np
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir) 
+
+import ez_spectral_analysis as ezpz
 import pytest
-import ../ez_spectral_analysis as ezpz
 
 def test_angstrom_units():
 
     # read in an example spectrum
-    path_to_fits_files ='data/s2d/' 
+    path_to_fits_files ='spectra_ez_download/data/s2d/' 
     fits_files = os.listdir(path_to_fits_files)
 
     # Read .fits data
@@ -13,5 +22,7 @@ def test_angstrom_units():
 
     for wavelength in wavelengths:
         min_wavel, max_wavel = np.min(wavelengths), np.max(wavelengths)
-        assert min_wavel >= 1000
-        assert max_wavel <= 10000
+        assert min_wavel >= 1000, "Wavelengths need to be in angstrom. Please convert your wavelengths."
+        assert max_wavel <= 10000, "Wavelengths need to be in angstrom. Please convert your wavelengths."
+
+
